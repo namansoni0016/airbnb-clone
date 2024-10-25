4cfd5c214df2d5cdaf4ffaffe8f99a671203758c
Naman Soni <namantsoni@gmail.com>
2024-10-25 09:45:56 +0530

    Login and Register Pages

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const IndexPage = () => {
+    return (
+        <div>
+            Index Page
+        </div>
+    )
+}
+
+export default IndexPage;
